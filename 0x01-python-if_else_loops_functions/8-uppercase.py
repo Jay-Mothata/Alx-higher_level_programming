a989eb4880a1e1a142eac91638893f20e75c9943
jay-mothata <joey.mothata@gmail.com>
2024-01-03 15:23:40 -0800

    Prints a string in uppercase.

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def uppercase(str):
+    for c in str:
+        i = ord(c)
+        if i >= 97 and i <= 122:
+            i -= 32
+        print("{:c}".format(i), end='')
+    print()
