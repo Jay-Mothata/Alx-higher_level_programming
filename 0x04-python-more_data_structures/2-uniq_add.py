5ec0e067e8b92dbf47a747376399197eb89ecb7b
jay-mothata <joey.mothata@gmail.com>
2024-01-12 14:36:24 -0800

    Adds all unique integers in a list (only once for each integer).

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -3,8 +3,8 @@
 def uniq_add(my_list=[]):
     """Add all unique integers in a list only once for each integer."""
     unique_set = set()
-    
+
     for num in my_list:
         unique_set.add(num)
-    
+
     return sum(unique_set)
